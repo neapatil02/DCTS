47606297466fb330fc6ade119640dd09f64ae1db
Patil, Neha Shekhar <nepatil@iu.edu>
2020-10-06 20:01:42 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/header/header.component';
+import { DashboardComponent } from './app/dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
